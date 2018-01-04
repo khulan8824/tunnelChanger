0547e625fd727082f8cbbf946d5e3b4e90511a66
khulan8824 <khulan.msc.2012@gmail.com>
2018-01-04 21:35:59 +0000

    Readin repeatedly from the internet

--- readServers.sh
+++ readServers.sh
@@ -10,5 +10,19 @@ while IFS= read -r proxy
 		curl_out=$(curl -w %{time_starttransfer},%{time_total},%{http_code},%{size_download} http://ovh.net/files/1Mb.dat -o /dev/null --interface ppp$i -s)
 		echo $i '>>>' $proxy
 		echo $current_date_time','$curl_out>>$proxy"_1mb"
+
+		curl_out=$(curl -w %{time_starttransfer},%{time_total},%{http_code},%{size_download} http://ovh.net/files/10Mb.dat -o /dev/null --interface ppp$i -s)
+		echo $current_date_time','$curl_out>>$proxy"_10mb"
+
+		curl_out=$(curl -w %{time_starttransfer},%{time_total},%{http_code},%{size_download} http://ovh.net/files/100Mb.dat -o /dev/null --interface ppp$i -s)
+		echo $current_date_time','$curl_out>>$proxy"_100mb"
+
+		curl_out=$(curl -w %{time_starttransfer},%{time_total},%{http_code},%{size_download} http://www,bbc.com -o /dev/null --interface ppp$i -s)
+		echo $current_date_time','$curl_out>>$proxy"_bbc"
+
+		curl_out=$(curl -w %{time_starttransfer},%{time_total},%{http_code},%{size_download} https://youtube.com -o /dev/null --interface ppp$i -s)
+		echo $current_date_time','$curl_out>>$proxy"_youtube"
+
 		i=$((i+1))
+
 	done <"$file"
---
+++ repeatServer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do
+ ./readServers.sh
+ sleep 5m
+done
